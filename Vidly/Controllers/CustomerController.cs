65993e15ce6ad9ce4d4df31d2ecfff3b94eadbec
jarvis_1897 <ranjanritik1897@gmail.com>
2023-08-18 08:59:55 +0900

    Add project files.

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+	public class CustomerController : Controller
+	{
+		// GET: Customers
+		public ActionResult Customer()
+		{
+
+			var customers = new List<Customer>
+			{
+				//new Customer { Name = "John Smith" },
+				//new Customer { Name = "Mary Williams" }
+			};
+
+			return View(customers);
+		}
+
+		public ActionResult Detail(int id)
+		{
+			var customers = new List<Customer>
+			{
+				new Customer { Name = "John Smith", Id = 1},
+				new Customer { Name = "Mary Williams", Id = 2 }
+			};
+			var customer = customers.FirstOrDefault(c => c.Id == id);
+			if (customer != null)
+			{
+				return View(customer);
+			}
+
+			return HttpNotFound();
+		}
+	}
+}
